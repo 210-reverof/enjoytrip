8823d7b91d2f82b43d6e736e8169d47178f94bc5
이원영 <sheltonwon@gmail.com>
2023-05-01 01:07:23 +0900

    feat: id별 조회 기능 완료 close #4

--- src/main/java/ssafy/ws/trip/attraction/controller/AttractionController.java
+++ src/main/java/ssafy/ws/trip/attraction/controller/AttractionController.java
@@ -3,6 +3,7 @@ package ssafy.ws.trip.attraction.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import ssafy.ws.trip.attraction.service.AttractionService;
@@ -14,11 +15,10 @@ public class AttractionController {
 	@Autowired
 	AttractionService attractionService;
 	
-	@GetMapping("/one")
-	public ModelAndView one() throws Exception {
+	@GetMapping("/{id}")
+	public ModelAndView one(@PathVariable("id") int contentId) throws Exception {
 		ModelAndView mv = new ModelAndView();
-		System.out.println(attractionService.selectOne(125405));
-		
+		System.out.println(attractionService.selectOne(contentId));
 		
 		mv.setViewName("redirect:/");
 		return mv;
