25ab902d9e4be49e81e364a065b70aa3e9388e47
이원영 <sheltonwon@gmail.com>
2023-05-11 17:57:59 +0900

    tmp: tmp commit

---
+++ frontend/src/components/LoginComp.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="text-end mx-5" id="header_nav_confirm_off">
+        <!-- <button @click="show" class="btn fs-5" style="color: SeaGreen">
+            로그인
+        </button>
+        <LoginModal :visible="loginModal" @close="loginModal = false" />
+        <button type="button" class="btn fs-5" style="color: Plum" id="btnsignin" data-bs-toggle="modal" data-bs-target="#signinModal">
+            회원가입
+        </button> -->
+        
+    </div>
+</template>
+
+<script>
+import LoginModal from '@/components/LoginModal.vue';
+export default {
+    name: "LoginComp",
+    components: {
+        LoginModal
+    },
+    data() {
+        return {
+            loginModal: false
+        };
+    },
+    methods: {
+        show() {
+            this.loginModal = true;
+            console.log(this.loginModal + "==");
+        }
+    }
+}
+
+</script>
---
+++ frontend/src/components/LoginModal.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="modal" :class="{'is-active': visible}">
+      <div class="modal-background"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">로그인</p>
+          <button class="delete" aria-label="close" @click="$emit('close')"></button>
+        </header>
+        <section class="modal-card-body">
+          <div class="field">
+            <label class="label">ID</label>
+            <div class="control">
+              <input class="input" type="text" placeholder="아이디를 입력하세요">
+            </div>
+          </div>
+          <div class="field">
+            <label class="label">비밀번호</label>
+            <div class="control">
+              <input class="input" type="password" placeholder="비밀번호를 입력하세요">
+            </div>
+          </div>
+        </section>
+        <footer class="modal-card-foot">
+          <button class="button is-success">로그인</button>
+          <button class="button" @click="$emit('close')">취소</button>
+        </footer>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "LoginModal",
+    props: {
+        visible: {
+            type: Boolean,
+            required: true
+        }
+    },
+    created() {
+        console.log(this.visible);
+    }
+  };
+  </script>
