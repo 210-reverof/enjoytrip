329a942c01a17dea2be7c3ea84b2d3d6305fabe8
이원영 <sheltonwon@gmail.com>
2023-05-11 15:22:07 +0900

    feat: 우리 지역 명소 완료 #14

---
+++ frontend/src/components/home/OurAreaImg.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="row m-1">
+    <div class="col-3">
+      <div id="our-area-title" class="fs-2 text-start m-3">우리지역 관광지</div>
+      <div class="fs-5 text-start m-3">
+        우리지역의 숨어있는 아름다운 관광지를 알려드립니다. 관광지 주변의 맛집, 숙박업소와
+        여행코스, 지역 축제등을 보실수도 있습니다.
+      </div>
+    </div>
+
+    <div class="col-9">
+      <div id="our-trips-panel" class="row row-cols-1 row-cols-lg-3 m-3 flex-nowrap justify-content-md-center"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "OurAreaImg",
+  mounted() {
+    const script = document.createElement('script')
+    script.src = 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js'
+    document.head.appendChild(script)
+  },
+  created() {
+    this.getOurTrips();
+  },
+  methods: {
+    getOurTrips() {
+      let tripsUrl = "https://apis.data.go.kr/B551011/KorService1/areaBasedList1?serviceKey=M3KVNZZ77dEKVDzXocK%2BBbXaXghygQd25WflNl70WZA9pOgx2ihFZSzaJbarptjUqHil53iFe%2F1oNLZtbi0DHg%3D%3D&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=AppTest&_type=json&listYN=Y&arrange=A&contentTypeId=28&areaCode=3&sigunguCode=5";
+      fetch(tripsUrl)
+          .then((response) => response.json())
+          .then((data) => this.getResults(data));
+    },
+
+    getResults(result) {   
+      var tmp = result.response.body.items.item;
+
+      let sel = [true, false, true, false, true];
+      this.shuffle(sel);
+
+      for (var i = 0; i < tmp.length; i++) {
+        if (sel[i]) {
+          this.addCard(tmp[i]);
+        }
+      }
+    },
+    
+    addCard(result) {
+      var cardPanel = document.getElementById("our-trips-panel");
+
+      var cardBodyEl = document.createElement("div");
+      cardBodyEl.setAttribute("class", "m-3");
+      cardBodyEl.setAttribute("style", "box-shadow: 0px 0px 15px grey");
+      cardBodyEl.setAttribute("data-bs-toggle", "modal");
+      cardBodyEl.setAttribute("data-bs-target", "#area-trip-modal");
+
+      var imgEl = document.createElement("img");
+      imgEl.setAttribute("src", result.firstimage);
+      imgEl.setAttribute("class", "card-img-top");
+
+      cardBodyEl.appendChild(imgEl);
+      cardPanel.appendChild(cardBodyEl);
+    },
+
+    shuffle(a) {
+      var j, x, i;
+      for (i = a.length - 1; i > 0; i--) {
+          j = Math.floor(Math.random() * (i + 1));
+          x = a[i];
+          a[i] = a[j];
+          a[j] = x;
+      }
+      return a;
+    }
+  }
+}
+</script>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="home">
-    <carousel-img></carousel-img>
+    <div class="container text-center">
+      <carousel-img></carousel-img>
+      <our-area-img></our-area-img>
+    </div>  
   </div>
 </template>
 
 <script>
 import CarouselImg from '@/components/home/CarouselImg.vue';
+import OurAreaImg from '@/components/home/OurAreaImg.vue';
 export default {
   name: 'HomeView',
   components: {
-    CarouselImg
+    CarouselImg,
+    OurAreaImg
   }
 }
 </script>
