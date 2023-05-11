d71aa60b18271fb21e1e3bb67222df5a2a19a1f7
이원영 <sheltonwon@gmail.com>
2023-05-11 13:03:58 +0900

    env: add bootstrap #14

---
+++ frontend/src/components/ConfirmComp.vue
@@ -0,0 +1,37 @@
+<template>
+    <header class="p-3 text-white">
+        <h1>Header</h1>
+        <div class="container-fluid">
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0 align-items-center">
+                    <li>
+                    <a href="#" class="nav-link mx-5 p-3 ps-5 px-2 fs-1" style="color: SeaGreen">Enjoy Trip</a>
+                    </li>
+                    <li>
+                    <a href="#" class="nav-link ps-5 px-2 fs-5" style="color: ForestGreen">지역별여행지</a>
+                    </li>
+                    <li>
+                    <a href="#" class="nav-link ps-3 px-2 fs-5" style="color: SkyBlue">나의여행계획</a>
+                    </li>
+                    <li>
+                    <a href="#" class="nav-link ps-3 px-2 fs-5" style="color: Crimson">핫플자랑하기</a>
+                    </li>
+                    <li>
+                    <a href="#" class="nav-link ps-3 px-2 fs-5" style="color: Plum">여행정보공유</a>
+                    </li>
+                    <li>
+                    <a href="#" class="nav-link ps-3 px-2 fs-5" style="color: Plum">공지사항</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+
+export default {
+
+}
+
+</script>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <confirm-comp></confirm-comp>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import ConfirmComp from '@/components/ConfirmComp.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HelloWorld,
+    ConfirmComp
   }
 }
 </script>
