7ea13317d8a60128efd63d95098a89017c1ddea9
이원영 <sheltonwon@gmail.com>
2023-05-12 12:01:54 +0900

    fix: fix origin patterns

--- frontend/src/views/MyRouteView.vue
+++ frontend/src/views/MyRouteView.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="myroute">
     <p>내 여행 계획</p>
+    <my-route-list></my-route-list>
   </div>
 </template>
 
 <script>
+import MyRouteList from '@/assets/route/MyRouteList.vue';
 
 export default {
-  name: 'MyRouteView'
+  name: 'MyRouteView',
+  components: {
+    MyRouteList
+  }
 }
 </script>
